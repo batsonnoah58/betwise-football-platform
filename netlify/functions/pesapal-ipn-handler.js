47abff6cb59c6af848344d5057814afe6e8c3491
Batson Noah <batsonnoah58@gmail.com>
2025-07-13 18:25:12 +0300

    Delete netlify/functions/pesapal-ipn-handler.js

--- netlify/functions/pesapal-ipn-handler.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  // Parse and verify IPN from Pesapal
-  // Update your database accordingly
-  // Respond with 200 OK
-  return {
-    statusCode: 200,
-    body: "IPN received",
-  };
-}; 
